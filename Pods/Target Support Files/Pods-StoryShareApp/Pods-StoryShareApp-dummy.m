d361c6d2e23d5a1928a30ffdbe052d6dfdd45b9e
Robert Sato <robertssato@gmail.com>
2020-04-18 20:28:43 -0700

    working signup and login with saved email and password firebase backend

---
+++ Pods/Target Support Files/Pods-StoryShareApp/Pods-StoryShareApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StoryShareApp : NSObject
+@end
+@implementation PodsDummy_Pods_StoryShareApp
+@end
---
+++ StoryShareApp/Helpers/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  StoryShareApp
+//
+//  Created by Robert Sato on 4/18/20.
+//  Copyright © 2020 Robert Sato. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        static let homeViewController = "HomeVC"
+    }
+    
+}
